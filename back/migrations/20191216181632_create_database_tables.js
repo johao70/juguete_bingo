817031eefc1bd4f9a25fad6e7452b90ea398f106
Johao Perlaza <perlazajohao@gmail.com>
2020-01-04 09:32:30 -0500

    bd

--- back/migrations/20191216181632_create_database_tables.js
+++ back/migrations/20191216181632_create_database_tables.js
@@ -7,22 +7,15 @@ exports.up = function(knex, Promise) {
     table.string('tipo_persona_descripcion');
   })
 
-  .createTable( 'tipo_identificacion', function( table ) {
-    table.increments('tipo_identificacion_id');
-    table.string('tipo_identificacion_nombre').notNullable().unique();
-  })
-
-  .createTable( 'estado_persona', function( table ) {
-    table.increments('estado_persona_id');
-    table.string('estado_persona_nombre').notNullable().unique();
-    table.string('estado_persona_descripcion');
+  .createTable( 'cartilla', function( table ) {
+    table.increments('cartilla_id');
+    table.string('cartilla_nombre').notNullable().unique();
+    table.string('cartilla_codigo').notNullable().unique();
   })
 
   .createTable( 'persona', function( table ) {
     table.increments('persona_id');
-    table.integer('tipo_identificacion_id').references('tipo_identificacion_id').inTable('tipo_identificacion');
     table.integer('tipo_persona_id').references('tipo_persona_id').inTable('tipo_persona');
-    table.integer('estado_persona_id').references('estado_persona_id').inTable('estado_persona');
     table.string('persona_identificacion').notNullable().unique();
     table.string('persona_nombre').notNullable();
     table.string('persona_email').notNullable();
@@ -30,12 +23,18 @@ exports.up = function(knex, Promise) {
     table.string('persona_telefono').notNullable();
     table.string('persona_clave').notNullable();
   })
+
+  .createTable( 'persona_cartilla', function( table ) {
+    table.increments('persona_cartilla_id');
+    table.integer('persona_id').references('persona_id').inTable('persona');
+    table.integer('cartilla_id').references('cartilla_id').inTable('cartilla');
+  })
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema
+    .dropTableIfExists( 'persona_cartilla' )
     .dropTableIfExists( 'persona' )
-    .dropTableIfExists( 'estado_persona' )
-    .dropTableIfExists( 'tipo_identificacion' )
+    .dropTableIfExists( 'cartilla' )
     .dropTableIfExists( 'tipo_persona' )
 };
